96d846305c115cc88bb2d01bec38ff0491791de4
Nguyen Duc Lai <nguyen.duc.lai@sun-asterisk.com>
2022-12-28 16:55:49 +0700

    update

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -9,7 +9,7 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: MyStatefulWidget(),
       ),
@@ -44,102 +44,117 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       return Colors.red;
     }
 
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text.rich(
-                  TextSpan(
-                    text: '',
-                    children: <TextSpan> [
-                      TextSpan(text: 'CONN', style: TextStyle(color: Color(0xFFf12776), fontSize: 28, fontWeight: FontWeight.bold)),
-                      TextSpan(text: 'EXION', style: TextStyle(color: Color(0xFFf9fcff), fontSize: 28, fontWeight: FontWeight.bold))
-                    ]
-                  )
-                )),
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Find and Meet people \n\n around to find LOVE',
-                  style: TextStyle(fontSize: 18),
-                )),
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Sign In',
-                  style: TextStyle(fontSize: 18),
-                )),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter Email',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            Container(
-              child: Checkbox(
-                checkColor: Colors.white,
-                fillColor: MaterialStateProperty.resolveWith(getColor),
-                value: isChecked,
-                onChanged: (bool? value) {
-                  setState(() {
-                    isChecked = value!;
-                  });
-                },
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                //forgot password screen
-              },
-              child: const Text('Forgot Password',),
-            ),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  child: const Text('Login'),
-                  onPressed: () {
-                    print(nameController.text);
-                    print(passwordController.text);
-                  },
-                )
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return Material(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment(0.8, 1),
+            colors: <Color>[
+              Color.fromRGBO(254, 115, 72, 1),
+              Color.fromRGBO(245, 57, 107, 1)
+            ], // Gradient from https://learnui.design/tools/gradient-generator.html
+            tileMode: TileMode.mirror,
+          ),
+        ),
+        child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
               children: <Widget>[
-                const Text('Does not have account?'),
-                TextButton(
-                  child: const Text(
-                    'Sign in',
-                    style: TextStyle(fontSize: 20),
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text.rich(
+                        TextSpan(
+                            text: '',
+                            children: <TextSpan> [
+                              TextSpan(text: 'CONN', style: TextStyle(color: Color(0xFFf12776), fontSize: 28, fontWeight: FontWeight.bold)),
+                              TextSpan(text: 'EXION', style: TextStyle(color: Color(0xFFf9fcff), fontSize: 28, fontWeight: FontWeight.bold))
+                            ]
+                        )
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      'Find and Meet people \n\n around to find LOVE',
+                      style: TextStyle(fontSize: 18),
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      'Sign In',
+                      style: TextStyle(fontSize: 18),
+                    )),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter Email',
+                    ),
                   ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Checkbox(
+                    checkColor: Colors.white,
+                    fillColor: MaterialStateProperty.resolveWith(getColor),
+                    value: isChecked,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isChecked = value!;
+                      });
+                    },
+                  ),
+                ),
+                TextButton(
                   onPressed: () {
-                    //signup screen
+                    //forgot password screen
                   },
-                )
+                  child: const Text('Forgot Password',),
+                ),
+                Container(
+                    height: 50,
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: ElevatedButton(
+                      child: const Text('Login'),
+                      onPressed: () {
+                        print(nameController.text);
+                        print(passwordController.text);
+                      },
+                    )
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text('Does not have account?'),
+                    TextButton(
+                      child: const Text(
+                        'Sign in',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        //signup screen
+                      },
+                    )
+                  ],
+                ),
               ],
-            ),
-          ],
-        ));
+            ))
+      ),
+    );
   }
 }
